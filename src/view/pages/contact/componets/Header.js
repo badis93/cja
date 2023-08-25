dd882f65bb3ef2a64c0ebcd7340b110776da39ad
khairy11 <khaireddinebaatour@gmail.com>
2023-08-25 14:24:12 +0100

    build1

--- src/view/pages/contact/componets/Header.js
+++ src/view/pages/contact/componets/Header.js
@@ -3,18 +3,14 @@ import React from "react";
 const Header = () => {
   return (
     <>
-      <section
-        className="bg-half-170 bg-light d-table w-100"
-        style={{
-          background: 'url("assets/images/contact-detail.jpg") center center',
-        }}
-      >
+      <section className="bg-half-170 bg-light d-table w-100">
         <div className="bg-overlay bg-overlay-white" />
         <div className="container">
           <div className="row mt-5 justify-content-center">
             <div className="col-lg-12 text-center">
               <div className="pages-heading">
                 <h4 className="title mb-0">Contact Us</h4>
+                {/* <img src={require("../componets/1.jpg")} /> */}
               </div>
             </div>{" "}
             {/*end col*/}
