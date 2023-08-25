5ca4fe1c1bd24340953a829c4eb8a2bbc9d92b6e
khairy11 <khaireddinebaatour@gmail.com>
2023-08-25 14:11:41 +0100

    new build

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import App from "./App";
-import  "./assets/styles.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./assets/styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route element={<App />} path="/*" />
+    </Routes>
   </BrowserRouter>
 );
